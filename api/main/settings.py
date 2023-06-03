560a7db69e7574198a0821a07469bb0a4eea1114
Bskasan <b.kasan@yandex.com>
2023-06-03 21:48:28 +0300

    created tutorial app and updated installed apps

--- api/main/settings.py
+++ api/main/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # MyApps;
+    'tutorial',
+
+    #3rd Party Apps;
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
